525ef389e0a3ac1a258e11fca44d3b468d537393
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2023-12-04 12:07:53 +0100

    orai munka

---
+++ fajlkezeles5.py
@@ -0,0 +1,15 @@
+def otodik():
+    # beolvasás
+    lista = []
+    beFajl = open("fileok/dal.txt", "r", encoding="utf-8")
+    for sor in beFajl:
+        lista.append(sor.strip())
+    beFajl.close()
+
+    # lista kiírása
+    kiFajl = open("fileok/otodik.txt", "w", encoding="utf-8")
+    for index in range(len(lista)-1, -1, -1):
+            print(lista[index], file=kiFajl)
+            print(lista[index])
+        # kiFajl.erite(lista[index])
+    kiFajl.close()
---
+++ main.py
@@ -0,0 +1,13 @@
+#import fajlkezeles
+#fajlkezeles.elso()
+import fajlkezeles2
+import fajlkezeles3
+import fajlkezeles4
+import fajlkezeles5
+
+#fajlkezeles2.masodik()
+
+#fajlkezeles3.harmadik()
+#fajlkezeles4.negyedik()
+fajlkezeles5.otodik()
+
